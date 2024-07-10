20b1eed93ec120e32b4602cf7f88fb390c28505e
Franklin2023-119030 <franklinmamaniapaza88@gmail.com>
2024-07-09 22:01:09 -0500

    Se agrego la primera funcionalidad, agregar un contacto

---
+++ Gestion de contacto.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct ContactoEmail {
+    char nombresCompletos[100];
+    char sexo;
+    int edad;
+    char telefono[20];
+    char email[50];
+    char nacionalidad[50];
+};
+
+void agregarContacto(ContactoEmail contactos[], int &numContactos) {
+    ContactoEmail nuevoContacto;
+
+    cout<<"Ingrese nombres completos: ";
+    cin.ignore();
+    cin.getline(nuevoContacto.nombresCompletos, 100);
+
+    cout<<"Ingrese el sexo (M/F): ";
+    cin>>nuevoContacto.sexo;
+    
+    cout<<"Ingrese la edad: ";
+    cin>>nuevoContacto.edad;
+    cin.ignore(); 
+    
+    cout<<"Ingrese el telefono: ";
+    cin.getline(nuevoContacto.telefono, 20);
+    
+    cout<<"Ingrese el email: ";
+    cin.getline(nuevoContacto.email, 50);
+    
+    cout<<"Ingrese la nacionalidad: ";
+    cin.getline(nuevoContacto.nacionalidad, 50);
+
+    contactos[numContactos] = nuevoContacto;
+    numContactos++;
+    cout<<"Contacto fue agregado exitosamente.\n";
+}
+int main() {
+    ContactoEmail contactos[100]; 
+    int numContactos = 0;
+    int opcion;
+
+    do {
+        cout<<"\nGestion de Contactos\n";
+        cout<<"1. Ingresar un contacto\n";
+        cout<<"2. Eliminar un contacto\n";
+        cout<<"3. Mostrar listado general de contactos\n";
+        cout<<"4. Mostrar listado de contactos ordenado por servidor de correo\n";
+        cout<<"5. Salir\n";
+        cout<<"Ingrese una opcion: ";
+        cin>>opcion;
+
+        switch(opcion) {
+            case 1:
+                agregarContacto(contactos, numContactos);
+                break;	  
+            default:
+                cout << "Opcion invalida. Intente de nuevo.\n";
+                break;
+        }
+    } while(opcion != 5);
+
+    return 0;
+}
+
+
+
+
